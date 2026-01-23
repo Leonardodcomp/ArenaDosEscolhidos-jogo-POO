81a758852ed48cf2074324b43393a4d1c625e707
Alan Cristian <alan.dias@dcomp.ufs.br>
2026-01-23 19:54:36 -0300

    Update Combatente.java
    Fiz um sistema de cura, onde cada personagem pode se curar duas vezes, com os Estus.

--- Combatente.java
+++ Combatente.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public abstract class Combatente {
+public abstract class Combatente{
 
     //Atributos serão declarados como protected para serem usados por toda a package
 
@@ -8,6 +8,7 @@ public abstract class Combatente {
     protected int vida;
     protected int vidaMaxima;
     protected Random random; // Para fazer a matemática de aleatoridade
+    protected int estus = 2 ; // cada combatente terá dois Estur para se curar.
 
     public Combatente(int vida) {
         this.vida = vida;
@@ -20,11 +21,28 @@ public abstract class Combatente {
 
 
     // Logica base de como o combatente recebe dano
+    public void curar(){
+        //irá curar sempre 30 pontos de vida, sem aleatoriedade. 
+        if (estus>0){
+            if((this.vida) + 30 < this.vidaMaxima){//não permiter que fique com mais do que a vida inicial.
+                this.vida +=30;
+                this.estus -=1;
+            }else{
+                this.vida=vidaMaxima;
+                this.estus -=1;
+            }
+        }else{
+            System.out.println("Quantidade de Estus insuficiente.");
+        }
+    }
 
     public void receberDano(int dano) {
         this.vida -= dano;
-        if (this.vida < 0) this.vida = 0;
-        System.out.println("você recebeu " + dano + " de dano. Vida restante: " + this.vida);
+        if (this.vida < 0){
+            this.vida = 0;
+        }else{
+            System.out.println("você recebeu " + dano + " de dano. Vida restante: " + this.vida);
+        }
     }
 
     // Logica booleana para verificar se o combatente está vivo (Usar depois talvez?)
