606d6647d1778d303c8846b4138d86fab4dead1b
Alan Cristian <alan.dias@dcomp.ufs.br>
2026-01-24 17:48:27 -0300

    Update arena.java
    Alterei a imagem.

--- arena.java
+++ arena.java
@@ -1,27 +1,19 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class arena {
 
     public void telinha() {
+        JFrame tela = new JFrame("Malucos por RPG!");
+        tela.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        tela.setUndecorated(true);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JFrame tela = new JFrame("Malucos por RPG!");
-    tela.setExtendedState(JFrame.MAXIMIZED_BOTH);
-    tela.setUndecorated(true);
-    tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon areninha = new ImageIcon(getClass().getResource("/imagens_do_jogo/arena/fundo.jpg"));
 
-    int largura = Toolkit.getDefaultToolkit().getScreenSize().width;
-    int altura  = Toolkit.getDefaultToolkit().getScreenSize().height;
-
-    ImageIcon original = new ImageIcon(
-        getClass().getResource("/imagens_do_jogo/arena/fundo.png")
-    );
-
-    Image imagemRedimensionada = original.getImage()
-                .getScaledInstance(largura, altura, Image.SCALE_SMOOTH);
-    JLabel fundo = new JLabel(new ImageIcon(imagemRedimensionada));
+        JLabel fundo = new JLabel(areninha); 
 
         tela.setContentPane(fundo);
+        tela.setLayout(null);
         tela.setVisible(true);
     }
 
